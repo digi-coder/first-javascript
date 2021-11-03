ec630f2e5c7c841516b0d3e27c79161d4a685062
digi-coder <maurizio_de_luca@hotmail.com>
2021-11-02 20:51:53 -0700

    Edit index.html and js-file.js

--- js-file.js
+++ js-file.js
@@ -48,3 +48,24 @@ function alertFunction() {
     alert("YAY! YOU DID IT!");
   }
 
+  btn3.addEventListener('click', function (e) {
+    console.log(e);
+  });
+
+  btn2.addEventListener('click', function (e) {
+    //console.log(e.target);
+    e.target.style.background = 'blue';
+  });
+
+  //nodelist
+  // buttons is a node list. It looks and acts much like an array.
+    const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+    buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+    });
